56e4a9215390f1a113ccd271c4bd6590f2d118a8
Eliot BOUTHERIN <ebouther@e2r4p6.42.fr>
2016-03-29 14:27:28 +0200

    binary execution works

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: ebouther <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/28 17:37:30 by ebouther          #+#    #+#             */
-/*   Updated: 2016/03/29 12:13:03 by ebouther         ###   ########.fr       */
+/*   Updated: 2016/03/29 14:27:09 by ebouther         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,53 +25,71 @@ static int	ft_get_in_env(char *search, char **env)
 	return (-1);
 }
 
-static char	*ft_get_user_input(void)
+static char	**ft_get_user_input(pid_t pid)
 {
+	char	**arg;
 	char	*str;
 
-	str = NULL;
+	arg = NULL;
 	if (get_next_line(1, &str))
-		return (str);
+	{
+		if (ft_strlen((char *)(arg = ft_strsplit(str, ' '))) > 0)
+		{
+			if (ft_strcmp(arg[0], "exit") == 0)
+			{
+				kill(pid, SIGKILL);
+			}
+			else if (ft_strcmp(arg[0], "cd") == 0)
+			{
+				ft_printf("CD\n");
+			}
+			return (arg);
+		}
+	}
 	return (NULL);
+}
+
+static void	ft_find_and_exec_bin(char **input, char **env)
+{
+	char	*path;
+	char	**split;
+	int		i;
+
+	if ((i = ft_get_in_env("PATH=", env)) != -1)
+	{
+		split = ft_strsplit(env[i], ':');
+		while (*split != '\0')
+		{
+			if (access(path = ft_strjoin(*split, ft_strjoin("/", input[0])), X_OK) == 0)
+				execve(path, ft_strlen((char *)input) > 1 ? input : NULL, NULL);
+			split++;
+		}
+	}
 }
 
 int	main(int ac, char **av, char **env)
 {
-	pid_t	father;
-	char	*input;
-	int		i;
-	char	**split;
+	pid_t	pid;
+	char	**input;
 
 	(void)ac;
 	(void)av;
-	father = fork();
 
-	if (father > 0)
+	while (42)
 	{
-		wait(NULL);
-		ft_printf("test\n");
-	}
-	if (father == 0)
-	{
-		if ((input = ft_get_user_input()) != NULL)
+		pid = fork();
+		if (pid == 0)
 		{
-			ft_printf("INPUT: '%s'\n", input);
-			if ((i = ft_get_in_env("PATH", env)) != -1)
-			{
-				//Should open all paths in env and search for input
-				ft_printf("FOUND\n");
-				split = ft_strsplit(env[i], ':');
-				while (*split != '\0')
-				{
-					ft_printf("SPLIT\n");
-					if (ft_strstr(*split, input) != NULL)
-					{
-						ft_printf("OK\n");
-						execve(*split, av, NULL);
-					}
-					split++;
-				}
-			}
+			ft_printf("$> ");
+			if ((input = ft_get_user_input(pid)) != NULL)
+				ft_find_and_exec_bin(input, env);
+		}
+		else if (pid > 0)
+			wait(NULL);
+		else
+		{
+			ft_printf("Fork error.\n");
+			exit(-1);
 		}
 	}
 	return (0);
