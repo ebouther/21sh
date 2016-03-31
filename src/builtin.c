15da2ece089ef8884dca68b1210b05e1f682ef37
Eliot BOUTHERIN <ebouther@e2r5p5.42.fr>
2016-03-31 21:16:51 +0200

    update

--- src/builtin.c
+++ src/builtin.c
@@ -6,16 +6,32 @@
 /*   By: ebouther <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/31 19:20:29 by ebouther          #+#    #+#             */
-/*   Updated: 2016/03/31 19:55:51 by ebouther         ###   ########.fr       */
+/*   Updated: 2016/03/31 21:16:25 by ebouther         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static void	ft_new_env(char **arg, char ***env, int *modified, char *tmp, int pos, char **new_env)
+{
+	int		i;
+
+	i = 0;
+	while ((*env)[i])
+	{
+		if (i == pos)
+			new_env[i] = ft_strjoin_free(ft_strdup(tmp), ft_strdup(arg[2]));
+		else
+			new_env[i] = ft_strdup((*env)[i]);
+		if (*modified == 1)
+			ft_strdel((*env) + i);
+		i++;
+	}
+}
+
 static void	ft_setenv(char **arg, char ***env, int *modified)
 {
 	int		len;
-	int		i;
 	char	*tmp;
 	char	**new_env;
 	int		pos;
@@ -29,17 +45,7 @@ static void	ft_setenv(char **arg, char ***env, int *modified)
 		ft_printf("minishell: malloc: cannot allocate memory.\n");
 		exit(-1);
 	}
-	i = 0;
-	while ((*env)[i])
-	{
-		if (i == pos)
-			new_env[i] = ft_strjoin_free(ft_strdup(tmp), ft_strdup(arg[2]));
-		else
-			new_env[i] = ft_strdup((*env)[i]);
-		if (*modified == 1)
-			ft_strdel((*env) + i);
-		i++;
-	}
+	ft_new_env(arg, env, modified, tmp, pos, new_env);
 	if (*modified == 1)
 		free((void *)(*env));
 	if (pos == -1)
