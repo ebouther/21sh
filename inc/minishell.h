323dfc1989baaf8d71499ec496a0d4a81163b0b5
Eliot BOUTHERIN <ebouther@e3r8p3.42.fr>
2016-04-13 21:44:20 +0200

    norme ok

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,19 +6,19 @@
 /*   By: ebouther <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/28 17:39:03 by ebouther          #+#    #+#             */
-/*   Updated: 2016/04/13 21:38:50 by ebouther         ###   ########.fr       */
+/*   Updated: 2016/04/13 21:44:09 by ebouther         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-#include <unistd.h>
-#include <stdio.h>
-#include <signal.h>
-#include "libft.h"
+# include <unistd.h>
+# include <stdio.h>
+# include <signal.h>
+# include "libft.h"
 
-typedef struct s_main
+typedef struct	s_main
 {
 	pid_t	pid;
 	char	**input;
@@ -26,8 +26,7 @@ typedef struct s_main
 	char	mode;
 }				t_main;
 
-
-typedef struct s_input
+typedef struct	s_input
 {
 	char		**ret;
 	char		**arg;
@@ -67,26 +66,26 @@ typedef struct	s_setenv
 /*
 ** builtin.c
 */
-void		ft_modify_env(char **arg, char ***env, int mode);
-void		ft_print_env(char **env);
+void			ft_modify_env(char **arg, char ***env, int mode);
+void			ft_print_env(char **env);
 
 /*
 ** browser.c
 */
-void		ft_open_dir(char *dir, char ***env);
-int			ft_open_home_dir(char **env);
-void		ft_change_directory(char **arg, char ***env);
+void			ft_open_dir(char *dir, char ***env);
+int				ft_open_home_dir(char **env);
+void			ft_change_directory(char **arg, char ***env);
 
 /*
 ** env.c
 */
-char		**ft_env_command(char *mode, int len, char **arg, char ***env);
-int			ft_get_in_env(char *search, char **env);
-void		ft_parse_args_for_env_var(char ***arg, char ***env);
+char			**ft_env_command(char *mode, int len, char **arg, char ***env);
+int				ft_get_in_env(char *search, char **env);
+void			ft_parse_args_for_env_var(char ***arg, char ***env);
 
 /*
 ** input.c
 */
-char		**ft_get_user_input(char *mode, char ***env);
+char			**ft_get_user_input(char *mode, char ***env);
 
 #endif
