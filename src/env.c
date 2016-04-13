e70981da1d5458b2df157c804cc53ed827ecc73f
Eliot BOUTHERIN <ebouther@e3r8p3.42.fr>
2016-04-13 20:36:27 +0200

    norme++

--- src/env.c
+++ src/env.c
@@ -6,13 +6,13 @@
 /*   By: ebouther <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/31 19:28:25 by ebouther          #+#    #+#             */
-/*   Updated: 2016/04/13 20:22:18 by ebouther         ###   ########.fr       */
+/*   Updated: 2016/04/13 20:36:08 by ebouther         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int		ft_get_in_env(char *search, char **env)
+int			ft_get_in_env(char *search, char **env)
 {
 	int		i;
 	char	*ret;
@@ -25,29 +25,33 @@ int		ft_get_in_env(char *search, char **env)
 	return (-1);
 }
 
-void	ft_parse_args_for_env_var(char ***arg, char ***env)
+static void	ft_replace_by_env_var(char ***arg, char ***env, int i, int pos)
+{
+	int		len;
+	char	*tmp;
+
+	len = ft_strlen((*arg)[i]);
+	if ((pos = ft_get_in_env(
+		tmp = ft_strjoin((*arg)[i] + 1, "="), *env)) != -1)
+	{
+		ft_strdel((*arg) + i);
+		(*arg)[i] = ft_strdup((*env)[pos] + len);
+	}
+	ft_strdel(&tmp);
+}
+
+void		ft_parse_args_for_env_var(char ***arg, char ***env)
 {
 	int		i;
 	int		pos;
-	int		len;
 	char	*tmp;
 
 	i = 0;
 	tmp = NULL;
 	while ((*arg)[i])
 	{
-		len = 0;
 		if ((*arg)[i][0] == '$')
-		{
-			len = ft_strlen((*arg)[i]);
-			if ((pos = ft_get_in_env(
-				tmp = ft_strjoin((*arg)[i] + 1, "="), *env)) != -1)
-			{
-				ft_strdel((*arg) + i);
-				(*arg)[i] = ft_strdup((*env)[pos] + len);
-			}
-			ft_strdel(&tmp);
-		}
+			ft_replace_by_env_var(arg, env, i, pos);
 		else if ((*arg)[i][0] == '~')
 		{
 			if ((pos = ft_get_in_env("HOME=", *env)) != -1)
@@ -57,6 +61,9 @@ void	ft_parse_args_for_env_var(char ***arg, char ***env)
 						ft_strdup(tmp + 1));
 				ft_strdel(&tmp);
 			}
+			else
+				printf("minishell: set $HOME environment \
+variable before using ~\n");
 		}
 		i++;
 	}
